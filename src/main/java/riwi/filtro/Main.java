24b3907e5721260edbd9eff8ae32d454fe48cf5c
= <=juanjoarenas1218@gmail.com>
2024-08-05 17:06:35 -0500

    A

--- src/main/java/riwi/filtro/Main.java
+++ src/main/java/riwi/filtro/Main.java
@@ -6,5 +6,6 @@ import riwi.filtro.persistence.dbConnection.Connect;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("HOLA");
     }
 }
